1d307f6a57123801d6d97136872d227580f25df0
Juca <clovis.junior@baritecnologia.com.br>
2023-12-18 09:06:13 -0300

    fix: assync emitter

--- src/emitter/index.ts
+++ src/emitter/index.ts
@@ -6,8 +6,12 @@ abstract class BaseEmitter {
   pub(id: string, ...args: any[]) {
     return emitter.emit(id, args);
   }
-  sub(id: string, listener: (...args: any[]) => void) {
-    return emitter.on(id, listener);
+  async sub(id: string, listener: (...args: any[]) => Promise<void> | void) {
+    return new Promise((resolve, reject) => {
+      emitter.on(id, async (args) => {
+        Promise.resolve(listener(args)).then(resolve).catch(reject);
+      });
+    });
   }
 }
 
