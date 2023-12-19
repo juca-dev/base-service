9cec6b2ab6605fc8b043543aea6ea13b3eb63283
Juca <clovis.junior@baritecnologia.com.br>
2023-12-19 12:03:57 -0300

    stable params

--- src/emitter/index.ts
+++ src/emitter/index.ts
@@ -9,14 +9,14 @@ abstract class BaseEmitter {
     });
   }
 
-  async pub(id: string, ...args: any[]) {
+  async pub(id: string, args: any) {
     const res = emitter.emit(id, args);
     console.log("pub", { res, args });
     if (res) {
       await this.delay(150); // delay to wait subscribed
     }
   }
-  async sub(id: string, listener: (...args: any[]) => Promise<void> | void) {
+  async sub(id: string, listener: (args: any) => Promise<void> | void) {
     return new Promise((resolve, reject) => {
       emitter.on(id, async (args) => {
         Promise.resolve(listener(args)).then(resolve).catch(reject);
