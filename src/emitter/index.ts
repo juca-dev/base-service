9998d26fa41bc09f9baec0298401d5752ee434a5
Juca <clovis.junior@baritecnologia.com.br>
2023-12-19 10:55:12 -0300

    delay to manager

--- src/emitter/index.ts
+++ src/emitter/index.ts
@@ -3,8 +3,18 @@ import EventEmitter from "events";
 const emitter = new EventEmitter();
 
 abstract class BaseEmitter {
-  pub(id: string, ...args: any[]) {
-    return emitter.emit(id, args);
+  private async delay(ms: number) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  async pub(id: string, ...args: any[]) {
+    const res = emitter.emit(id, args);
+    console.log("pub", res);
+    if (res) {
+      await this.delay(150); // delay to wait subscribed
+    }
   }
   async sub(id: string, listener: (...args: any[]) => Promise<void> | void) {
     return new Promise((resolve, reject) => {
